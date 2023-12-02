cd6b821df9d7f7befabeee3c1b732ffac0540f85
Nikita Zhukov <82539366+necalec@users.noreply.github.com>
2023-12-03 01:57:48 +0300

    поправил

--- threads.py
+++ threads.py
@@ -3,11 +3,13 @@ import time
 import csv
 import psutil
 
-def get_metric(point, named, n, lock: Lock):
+def get_metric(named, n, lock: Lock):
     for i in range(0, n):
-        print(point)
         now_time = (time.strftime('%d.%m.%Y %H:%M:%S', time.localtime(time.time())))
-        points = [now_time, point, named]
+        if named == 'cpu':
+            points = [now_time, psutil.cpu_freq().current, 'CPU frequency']
+        if named == 'ram':
+            points = [now_time, round((psutil.virtual_memory().used)/8388608, 2), 'RAM using (MB)']
         lock.acquire()
         with open('base.csv', 'a') as file:
             writer = csv.writer(file)
@@ -19,8 +21,8 @@ def get_metric(point, named, n, lock: Lock):
 #n = int(input("minutes: "))
 n = 5
 lock = Lock()
-t1 = Thread(target=get_metric, args=(psutil.cpu_freq().current,'CPU frequency', n, lock,), daemon=True)
-t2 = Thread(target=get_metric, args=(round((psutil.virtual_memory().used)/8388608, 2),'RAM using (MB)', n, lock,), daemon=True)
+t1 = Thread(target=get_metric, args=('cpu', n, lock,), daemon=True)
+t2 = Thread(target=get_metric, args=('ram', n, lock,), daemon=True)
 t1.start()
 t2.start()
 t1.join()
