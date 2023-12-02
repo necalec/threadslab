885ba8d2a609f94bcb95b5cafdd3bddc8aaafc71
Nikita Zhukov <82539366+necalec@users.noreply.github.com>
2023-12-02 23:03:37 +0300

    new

--- threads.py
+++ threads.py
@@ -3,23 +3,25 @@ import time
 import csv
 import psutil
 
-def get_metric(point, named, lock: Lock):   #используем одну функция для 2-х метрик, т.к. меняется лишь один аргумент
-    print(point)
-    now_time = (time.strftime('%d.%m.%Y %H:%M:%S', time.localtime(time.time())))
-    points = [now_time, point, named]
-    with open('base.csv', 'a') as file:     #добавляем массив значений в файл каждую минуту
-        writer = csv.writer(file)
-        writer.writerow(points)
+def get_metric(point, named, n, lock: Lock):
+    for i in range(0, n):
+        print(point)
+        now_time = (time.strftime('%d.%m.%Y %H:%M:%S', time.localtime(time.time())))
+        points = [now_time, point, named]
+        lock.acquire()
+        with open('base.csv', 'a') as file:
+            writer = csv.writer(file)
+            writer.writerow(points)
+        lock.release()
+        if i != n - 1:
+            time.sleep(60)
 
 #n = int(input("minutes: "))
 n = 5
-for i in range(0, n):
-    lock = Lock()                           #за 60 сек один поток успеет дождаться другой, но с куда меньшим временем это не работало, поэтому делаем лок
-    t1 = Thread(target=get_metric, args=(psutil.cpu_freq().current,'CPU frequency', lock,), daemon=True)
-    t2 = Thread(target=get_metric, args=(round((psutil.virtual_memory().used)/8388608, 2),'RAM using (MB)', lock,), daemon=True)
-    t1.start()
-    t2.start()
-    if i != n:                              #если постать слип тут, то всё будет секнда в секунду
-        time.sleep(60)
+lock = Lock()
+t1 = Thread(target=get_metric, args=(psutil.cpu_freq().current,'CPU frequency', n, lock,), daemon=True)
+t2 = Thread(target=get_metric, args=(round((psutil.virtual_memory().used)/8388608, 2),'RAM using (MB)', n, lock,), daemon=True)
+t1.start()
+t2.start()
 t1.join()
 t2.join()
